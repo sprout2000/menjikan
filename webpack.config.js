b570fdd7c78809574706e1317ab3476fd028f350
sprout2000 <office@nishigami.jp>
2020-09-25 15:52:49 +0900

    chore: delete webpack.config.prod.js

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
 
+const isDev = process.env.NODE_ENV === 'development';
+
 /** @type import('webpack').Configuration */
 module.exports = {
-  mode: 'development',
+  mode: isDev ? 'development' : 'production',
   entry: {
     app: './src/App.tsx',
   },
@@ -36,24 +38,35 @@ module.exports = {
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      favicon: './src/favicon.ico',
-    }),
-    new CopyWebpackPlugin([
-      {
-        from: 'assets',
-        to: './',
-        toType: 'dir',
-      },
-    ]),
-  ],
+  plugins: isDev
+    ? [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          favicon: './src/favicon.ico',
+        }),
+        new CopyWebpackPlugin({
+          patterns: [{ from: 'assets', to: '.' }],
+        }),
+      ]
+    : [
+        new HtmlWebpackPlugin({
+          template: './src/index.html',
+          favicon: './src/favicon.ico',
+        }),
+        new CopyWebpackPlugin({
+          patterns: [{ from: 'assets', to: '.' }],
+        }),
+        new WorkboxWebpackPlugin.GenerateSW({
+          swDest: 'service-worker.js',
+          skipWaiting: true,
+          clientsClaim: true,
+        }),
+      ],
   performance: {
     hints: false,
   },
   stats: 'minimal',
-  devtool: 'inline-source-map',
+  devtool: isDev ? 'inline-source-map' : false,
   devServer: {
     contentBase: path.resolve(__dirname, 'dist'),
     port: 1234,
