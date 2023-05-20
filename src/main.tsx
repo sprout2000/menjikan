a0ea7fb730a75e21c0d4018f2f595eac98b1a487
sprout2000 <wassabie64@gmail.com>
2023-05-20 09:50:03 +0900

    feat: Update to Roboto font

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 
 import { App } from "./App";
 
-import "@fontsource/chivo";
 import { registerSW } from "virtual:pwa-register";
 
 createRoot(document.getElementById("root") as Element).render(
